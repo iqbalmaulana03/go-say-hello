b5a7d78dfee009f6cdfb2ea5168f255040dee2c1
lenovo <maulanamasiqbal@gmail.com>
2023-07-31 23:28:13 +0700

    update  Say with parameter name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/iqbalmaulana03/go-say-hello
+module github.com/iqbalmaulana03/go-say-hello/v2
 
 go 1.20
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package go_say_hello
 
-func Sayhello() string {
+func Sayhello(name string) string {
 	return "Hello Word"
 }
